2df13a86f02b6734082da5a1721254e710fea63d
thomashamer3 <60607850+thomashamer3@users.noreply.github.com>
2024-04-11 11:46:25 -0300

    Ejercicio01 LABO4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'clase_1';
+  title = 'primera-app-angular';
+
+  edadUno: number = 0;
+  edadDos: number = 0;
+  promedioEdad: number = 0;
+  sumaEdad: number = 0;
+
+  calcular() {
+      this.sumar();
+      this.promedio();
+  }
+
+promedio() {
+  this.promedioEdad = (this.edadUno + this.edadDos) / 2;
+}
+
+sumar():void{
+  this.sumaEdad = this.edadUno + this.edadDos;
+}
+
+limpiar() {
+  this.edadUno = 0;
+  this.edadDos = 0;
+  this.promedioEdad = 0;
+  this.sumaEdad = 0;
+}
 }
